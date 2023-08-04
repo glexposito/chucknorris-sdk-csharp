b2862d83fb7bac73e0172bdeb4cdc7c30a693870
Guillermo <glexposito@gmail.com>
2023-08-04 22:27:02 +1200

    Implement calls to chucknorris.io API (#1)
    - Implement calls to chucknorris.io API
    - Add very basic tests

---
+++ src/ChuckNorris.Sdk.Infrastructure/ChuckJoke.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ChuckNorris.Sdk.Infrastructure;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public record ChuckJoke {
+    [JsonPropertyName("categories")]
+    public required IEnumerable<string> Categories { get; init; }
+    
+    [JsonPropertyName("created_at")]
+    public required string CreateadAt { get; init; }
+    
+    [JsonPropertyName("icon_url")]
+    public required string IconUrl { get; init; }
+    
+    [JsonPropertyName("id")]
+    public required string Id { get; init; }
+    
+    [JsonPropertyName("updated_at")]
+    public required string UpdatedAt { get; init; }
+    
+    [JsonPropertyName("url")]
+    public required string  Url { get; init; }
+    
+    [JsonPropertyName("value")]
+    public required string Value { get; init; }
+}
---
+++ src/ChuckNorris.Sdk.Infrastructure/TextSearchResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ChuckNorris.Sdk.Infrastructure;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public record TextSearchResponse {
+    
+    [JsonPropertyName("total")]
+    public int Total { get; init; }
+    
+    [JsonPropertyName("result")]
+    public required IEnumerable<ChuckJoke> Result { get; init; }
+}
