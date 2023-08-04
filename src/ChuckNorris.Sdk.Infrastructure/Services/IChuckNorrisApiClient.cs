0ea560be1b9c017dcf06fbb2259f879f2f579d2a
Guillermo <glexposito@gmail.com>
2023-08-04 22:26:08 +1200

    Implement calls to chucknorris.io API
    - Implement calls to chucknorris.io API
    - Add very basic tests

---
+++ src/ChuckNorris.Sdk.Infrastructure/Services/IChuckNorrisApiClient.cs
@@ -0,0 +1,12 @@
+﻿namespace ChuckNorris.Sdk.Infrastructure.Services;
+
+public interface IChuckNorrisApiClient
+{
+    Task<IEnumerable<string>> GetCategoriesAsync(CancellationToken cancellationToken = default);
+    
+    Task<ChuckJoke> GetRandomChuckJokeAsync(CancellationToken cancellationToken = default);
+    
+    Task<ChuckJoke> GetChuckJokeByCategoryAsync(string category, CancellationToken cancellationToken = default);
+    
+    Task<TextSearchResponse> SearchChuckJokeByTextAsync(string text, CancellationToken cancellationToken = default);
+}
