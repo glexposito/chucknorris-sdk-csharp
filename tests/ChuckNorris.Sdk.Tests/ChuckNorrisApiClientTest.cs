e33e935f9b32e9bdf0bbaad189529542be4af9fe
Guillermo <glexposito@gmail.com>
2023-08-04 22:37:40 +1200

    - Add very basic API client tests (#2)

---
+++ tests/ChuckNorris.Sdk.Tests/ChuckNorrisApiClientTest.cs
@@ -0,0 +1,81 @@
+using ChuckNorris.Sdk.Infrastructure.Services;
+using FluentAssertions;
+
+namespace ChuckNorris.Sdk.Tests;
+
+public class ChuckNorrisApiClientTest
+{
+    private readonly ChuckNorrisApiClient _client = new();
+
+    [Fact]
+    public async void GetCategoriesAsync_ShouldReturnCategories()
+    {
+        var categories = await _client.GetCategoriesAsync();
+
+        categories.Should().NotBeEmpty();
+    }
+    
+    [Fact]
+    public async void GetRandomChuckJokeAsync_ShouldReturnChuckJoke()
+    {
+        var chuckJoke = await _client.GetRandomChuckJokeAsync();
+
+        chuckJoke.Categories.Should().BeEmpty();
+        chuckJoke.CreateadAt.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.IconUrl.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.Id.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.UpdatedAt.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.Url.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.Value.Should().NotBeNullOrWhiteSpace();
+    }
+    
+    [Fact]
+    public async void GetChuckJokeByCategoryAsync_WithValidCategory_ShouldReturnChuckJoke()
+    {
+        var chuckJoke = await _client.GetChuckJokeByCategoryAsync("dev");
+
+        chuckJoke.Categories.Should().NotBeEmpty();
+        chuckJoke.CreateadAt.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.IconUrl.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.Id.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.UpdatedAt.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.Url.Should().NotBeNullOrWhiteSpace();
+        chuckJoke.Value.Should().NotBeNullOrWhiteSpace();
+    }
+    
+    [Fact]
+    public async void SearchChuckJokeByTextAsync_WithExistingText_ShouldReturnChuckJokes()
+    {
+        var response = await _client.SearchChuckJokeByTextAsync("dev");
+
+        response.Total.Should().BePositive();
+
+        foreach (var chuckJoke in response.Result)
+        {
+            chuckJoke.CreateadAt.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.IconUrl.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.Id.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.UpdatedAt.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.Url.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.Value.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+    
+    [Fact]
+    public async void SearchChuckJokeByTextAsync_WithNonExistingText_ShouldNotReturnChuckJokes()
+    {
+        var response = await _client.SearchChuckJokeByTextAsync("gu1ll3e");
+
+        response.Total.Should().Be(0);
+
+        foreach (var chuckJoke in response.Result)
+        {
+            chuckJoke.CreateadAt.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.IconUrl.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.Id.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.UpdatedAt.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.Url.Should().NotBeNullOrWhiteSpace();
+            chuckJoke.Value.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
