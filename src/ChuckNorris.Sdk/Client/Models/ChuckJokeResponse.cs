14570d2ab47fc5c1cd34d578b790ea1a31b09696
Guillermo <glexposito@gmail.com>
2023-08-20 10:32:16 +1200

    Update client SDK implementation
    - Update client SDK implementation
    - Change response models
    - Update tests

---
+++ src/ChuckNorris.Sdk/Client/Models/ChuckJokeResponse.cs
@@ -0,0 +1,8 @@
+﻿using ChuckNorris.Sdk.Infrastructure.Services.Models;
+
+namespace ChuckNorris.Sdk.Client.Models;
+
+public class ChuckJokeResponse : ChuckNorrisApiResponse
+{
+    public ChuckJoke? ChuckJoke { get; set; }
+}
